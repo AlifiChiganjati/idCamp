f10841288a8f5b9dcd61ea6ee2c1edd62e3b3f2e
alifichiganjati <alifichiganjati@gmail.com>
2023-10-20 15:57:25 -0400

    feat: navbar responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 const navbarMobile = document.querySelector(".navbar-menu__mobile");
+const navbar = document.querySelector(".navbar-wrapper");
+const sections = document.querySelectorAll("section");
+const navbarLinks = document.querySelectorAll(".navbar-wrapper li a");
 
 navbarMobile.addEventListener("click", () => {
-  console.log("hola");
+  navbar.classList.toggle("hidden");
+});
+
+window.addEventListener("scroll", () => {
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 150;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navbarLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector(".navbar-wrapper li a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 });
